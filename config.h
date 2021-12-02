a89911f937c350826c63164405633f27f7d28020
adampodkdev@gmail.com <adampodkdev@gmail.com>
2021-12-02 21:43:12 +0100

    change theme

--- config.h
+++ config.h
@@ -4,14 +4,14 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"FiraCode Nerd Font:size=10",
+	"Fira Code Retina:size=10:antialias=true:autohint=true",
 	"Noto Color Emoji:size=9"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#2f78f5" },
+	[SchemeSel] = { "#323034", "#aeb0e0" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
