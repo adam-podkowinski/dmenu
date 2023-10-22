7ce91877340fabcd1ec942367c12053785ff9a05
Adam Podkowinski <adampodkev@gmail.com>
2023-10-22 14:02:09 +0200

    change color

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#323034", "#aeb0e0" },
+	[SchemeSel] = { "#323034", "#c293fb" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
